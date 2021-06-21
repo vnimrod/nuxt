971978fc43d558e12618986fceae4408d49a520e
nimrod vain <togithub1@gmail.com>
2021-06-21 17:01:51 +0300

    first commit

---
+++ client/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Nav />
+    <div class="container">
+      <Hero />
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { largeCardSections } from "../assets/data";
+import { smallCardSections } from "../assets/data";
+export default {
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections
+    };
+  }
+};
+</script>
+
+<style></style>
---
+++ client/pages/my-items.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    jsadbf
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
